76a252d4817f9f839aba9fccc8a5df918582b0c6
Michael Summers <student@epicodus.com>
2015-08-21 10:20:18 -0700

    Add spec to test client :id

--- spec/clients_spec.rb
+++ spec/clients_spec.rb
@@ -7,5 +7,10 @@ describe(Clients) do
   #     expect().to eq()
   #   end
   # end
-  
+  describe('#id') do
+    it('returns id of a new client') do
+      new_client = Clients.new({:id =>nil, :name => "Michael", :email => "some_gmail@gmail.com", :phone =>"111-111-1111", :address=>"404 Yellow Brick Rd.", :city => "Wizard_of_Oz", :zip =>00001, :stylist_id =>nil})
+      expect(new_client.id()).to eq(nil)
+    end
+  end
 end
