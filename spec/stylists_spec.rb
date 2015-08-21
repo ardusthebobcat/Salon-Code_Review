8be58979a1a44b1e3809eea656a6a94781cfe5ac
Michael Summers <student@epicodus.com>
2015-08-21 10:14:03 -0700

    Add spec to test client id (currently empty), pass.

--- spec/stylists_spec.rb
+++ spec/stylists_spec.rb
@@ -1,5 +1,4 @@
-require('rspec')
-require('stylists')
+require('spec_helper')
 
 describe(Stylists) do
   ## spec-template
@@ -13,6 +12,19 @@ describe(Stylists) do
     it('returns the name of a new stylist') do
       new_stylist = Stylists.new({:id => nil, :name => "Darren"})
       expect(new_stylist.name()).to eq("Darren")
+    end
+  end
+
+  describe('#client_id') do
+    it('returns the ids of clients assigned to a stylist, currently empty') do
+      new_stylist = Stylists.new({:id => nil, :name => "Sue"})
+      expect(new_stylist.client_id()).to eq([])
+    end
+
+    it('returns the ids of clients assigned to a stylist') do
+      new_client = Clients.new({:id =>nil, :name => "Michael", :email => "some_gmail@gmail.com", :phone =>"111-111-1111", :address=>"404 Yellow Brick Rd.", :city => "Wizard_of_Oz", :zip =>00001, :stylist_id =>nil})
+      new_stylist = Stylists.new({:id => nil, :name => "Sue", :client_id => new_client})
+      expect(new_stylist.client_id()).to eq([new_client])
     end
   end
 end
