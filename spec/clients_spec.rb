c522418619ffff17e97cf01ad1448e07e8f55354
Michael Summers <student@epicodus.com>
2015-08-21 10:30:08 -0700

    Add spec to test client :email.

--- spec/clients_spec.rb
+++ spec/clients_spec.rb
@@ -4,7 +4,7 @@ describe(Clients) do
   ## spec-template
   # describe('#') do
   #   it('') do
-  #new_client = Clients.new({:id =>nil, :name => "", :email => "@gmail.com", :phone =>"111-111-1111", :address=>"", :city => "", :zip =>00002, :stylist_id =>nil})
+  # new_client = Clients.new({:id =>nil, :name => "", :email => "@gmail.com", :phone =>"111-111-1111", :address=>"", :city => "", :zip =>00002, :stylist_id =>nil})
   #     expect().to eq()
   #   end
   # end
@@ -19,6 +19,13 @@ describe(Clients) do
     it('returns name of a new client') do
       new_client = Clients.new({:id =>nil, :name => "Legolas", :email => "legolas_email@gmail.com", :phone =>"111-111-1111", :address=>"Near the forest", :city => "Rivendell", :zip =>00002, :stylist_id =>nil})
       expect(new_client.name()).to eq("Legolas")
+    end
+  end
+
+  describe('#email') do
+    it('returns Elrond"s email') do
+  new_client = Clients.new({:id =>nil, :name => "Elrond", :email => "I_live_long_time@gmail.com", :phone =>"111-111-1111", :address=>"My Palace", :city => "Rivendell", :zip =>00003, :stylist_id =>nil})
+      expect(new_client.email).to eq("I_live_long_time@gmail.com")
     end
   end
 end
